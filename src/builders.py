cedc6883b94c3d601d732961a9d6207245891b53
Live session user <abrobinson1907@gmail.com>
2020-07-31 19:21:55 +0000

    feat; add basic construction of part builders

--- src/builders.py
+++ src/builders.py
@@ -1,19 +1,67 @@
 import abc
+import src.parts
 
 
-class ABCVehicleBuilder(abc.ABC):
+class ABCBuilder(abc.ABC):
     '''
     Specifies methods for creating a builder
     '''
 
+    @abc.abstractmethod
+    def reset(self) -> None:
+        pass
+
     @abc.abstractproperty
-    def vehicle(self) -> None:
+    def product(self) -> None:
         pass
 
-    @abc.abstractmethod
-    def make_first_stage(self) -> None:
-        pass
 
-    @abc.abstractmethod
-    def make_second_stage(self) -> None:
-        pass
+class StageBuilder(ABCBuilder):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._stage = src.parts.Stage()
+
+    @property
+    def product(self) -> src.parts.Stage:
+        product = self._stage
+        self.reset()
+        return product
+
+    def build(self, product: str):
+        '''
+        Constructs stage from name of stage
+
+        Parameters
+        ----------
+        product: str
+            Name of the stage
+        '''
+        # TODO
+
+
+class VehicleBuilder(ABCBuilder):
+    def __init__(self) -> None:
+        self._stage_builder = StageBuilder()
+        self.reset()
+
+    def reset(self):
+        self._vehicle = src.parts.Vehicle()
+
+    @property
+    def product(self) -> src.parts.Vehicle:
+        product = self._vehicle
+        self.reset()
+        return product
+
+    def build(self) -> None:
+        '''
+        Constructs stage from name of stage
+
+        Parameters
+        ----------
+        product: str
+            Name of the stage
+        '''
+        # TODO
