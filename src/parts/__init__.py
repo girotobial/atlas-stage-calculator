fd38efbd6995006b503034ff5baaa07cdf33707b
opensomersault <abrobinson1907@gmail.com>
2020-08-29 11:21:51 +0100

    style: change to vertical list

--- src/parts/__init__.py
+++ src/parts/__init__.py
@@ -1,5 +1,12 @@
 from .engines import Engine
 from .assembly import Stage, Vehicle
 from .tanks import Tank
+from .coupler import Coupler
 
-__all__ = ['Engine', 'Stage', 'Vehicle', 'Tank']
+__all__ = [
+    'Engine',
+    'Stage',
+    'Vehicle',
+    'Tank',
+    'Coupler',
+]
